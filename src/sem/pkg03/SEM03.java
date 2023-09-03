6c56a0d1619502ad37a455b26ab339290cf915e7
Eduardo <Eduardo@WIN-7J7TDH5DR1T>
2023-09-03 14:13:49 -0500

    Laboratorio Semana 03

---
+++ src/sem/pkg03/SEM03.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sem.pkg03;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class SEM03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Inicio i = new Inicio();
+        i.setVisible(true);
+    }
+    
+}
